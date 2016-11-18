d341aa7181b7c5dfb127f0b74ad5c6720dc4a913
Bernd Kreuss <prof7bit@gmail.com>
2016-11-18 23:59:36 +0100

    port python test script to new 64 byte descriptors

--- hidtest.py
+++ hidtest.py
@@ -3,6 +3,14 @@
 # this requires cython-hidapi:
 # pip3 install hidapi
 import hid
+import os
+
+def send_hid(d, x):
+    if os.name == 'nt':
+        buf = [0] + x
+    else:
+        buf = x
+    return d.write(buf)
 
 
 def send_string(d, text):
@@ -20,8 +28,8 @@ def send_string(d, text):
     while len(x) < 64:
         x.append(0)
     
+    num = send_hid(d, x)
     print("    sent: " + text)
-    d.write(x)
 
 def recv_string(d):
     s = ""
@@ -39,14 +47,24 @@ def recv_string(d):
             
     return s
 
+def send_msg(d, b):
+    x = [0] * 64
+    x[0] = 255
+    x[1] = b
+    send_hid(d, x)
+
 def main():
+    toggle = 1
     d = hid.device()
     d.open(0xdead, 0xbeef)
 
     for i in range(100):
         if i % 10 == 0:
             send_string(d, "Hello world!")
-        print("received: " + recv_string(d))
+        if i % 11 == 0:
+            send_msg(d, toggle)
+            toggle = 1 - toggle
+        print("received: " + recv_string(d).strip())
 
     d.close()
 
